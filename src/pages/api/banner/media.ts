551e23586e1529b655305f47784b3676b0060435
Nguyễn Huy Khải <huykhai.work@gmail.com>
2025-03-09 12:48:27 +0700

    fix: fix bug call api banner

--- src/pages/api/banner/media.ts
+++ src/pages/api/banner/media.ts
@@ -24,7 +24,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       },
       take: 10,
       select: {
-        id: true,
         media_url: true,
         description: true,
         uploaded_by: true,
