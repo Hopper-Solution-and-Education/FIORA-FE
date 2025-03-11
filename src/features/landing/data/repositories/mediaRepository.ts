706d014469da82dffba8668f5050c200a268bdc7
Đinh Gia Bảo <107354883+RankillerDY@users.noreply.github.com>
2025-03-11 20:34:46 +0700

    feat: update code from develop

--- src/features/landing/data/repositories/mediaRepository.ts
+++ src/features/landing/data/repositories/mediaRepository.ts
@@ -1,8 +1,8 @@
-import { MediaType, SectionType } from '@prisma/client';
+import { SectionType } from '@prisma/client';
 
 import { IMediaRepository } from '../../domain/interfaces/MediaRepository';
 import { Media } from '../../domain/models/Media';
-import prisma from '@/infrastructure/database/prisma';
+import { fetchMedia } from '../api';
 
 export class MediaRepository implements IMediaRepository {
   private static instance: MediaRepository;
@@ -17,23 +17,6 @@ export class MediaRepository implements IMediaRepository {
   }
 
   async getMediaBySection(sectionType: SectionType): Promise<Media[]> {
-    const result = await prisma.media.findMany({
-      where: { section: { section_type: sectionType }, media_type: MediaType.IMAGE },
-      select: {
-        id: true,
-        media_type: true,
-        media_url: true,
-        embed_code: true,
-        description: true,
-        uploaded_by: true,
-        uploaded_date: true,
-        section_id: true,
-      },
-    });
-
-    return result.map((item) => ({
-      ...item,
-      section_type: sectionType,
-    }));
+    return await fetchMedia(sectionType);
   }
 }
