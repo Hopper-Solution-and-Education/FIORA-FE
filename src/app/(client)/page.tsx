83c3805e14c0cb27616a75e472d41a6b068b2809
Nguyễn Huy Khải <66985115+nguyenhuykhai@users.noreply.github.com>
2025-03-29 15:06:16 +0700

    fix: minor bug session redirect behavior (#77)

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import Loading from '@/components/common/atoms/Loading';
+import { useSession } from 'next-auth/react';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
 
 const LandingPageRender = dynamic(() => import('@/features/landing/presentation/LandingPage'), {
   loading: () => <Loading />,
@@ -8,6 +10,18 @@ const LandingPageRender = dynamic(() => import('@/features/landing/presentation/
 });
 
 const Page = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === 'loading') {
+    return <Loading />;
+  }
+
+  if (session?.user) {
+    router.push('/home');
+    return null;
+  }
+
   return <LandingPageRender />;
 };
 
