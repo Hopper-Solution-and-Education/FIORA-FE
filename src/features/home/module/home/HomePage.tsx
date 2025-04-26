557a76ced4b07791042e71ff74fd7b536e12b81c
Nguyễn Huy Khải <huykhai.work@gmail.com>
2025-04-26 16:59:09 +0700

    feat: update global stacked bar chart

--- src/features/home/module/home/HomePage.tsx
+++ src/features/home/module/home/HomePage.tsx
@@ -1,15 +1,20 @@
 'use client';
+import StackedBarChart from '@/components/common/stacked-bar-chart';
 import { Tabs, TabsContent } from '@/components/ui/tabs';
 import { MODULE } from '@/shared/constants';
+import { FeatureFlags } from '@/shared/constants/featuresFlags';
+import { useFeatureFlagGuard } from '@/shared/hooks/useFeatureFlagGuard';
+import { data2023, data2024, data2025 } from '@public/dummy/stack-bar-chart';
+import AccountDashboard from '../account/AccountDashboard';
 import { AccountsOverview } from './AccountOverview';
 import RecentTransactions from './components/RecentTransactions';
 import Recommendations from './components/Recommendations';
-import { FeatureFlags } from '@/shared/constants/featuresFlags';
-import { useFeatureFlagGuard } from '@/shared/hooks/useFeatureFlagGuard';
-import AccountDashboard from '../account/AccountDashboard';
+import { useAppSelector } from '@/store';
+import { formatCurrency } from '@/shared/utils';
 
 export default function HomePage() {
   const { isFeatureOn } = useFeatureFlagGuard(FeatureFlags.ACCOUNT_FEATURE, MODULE.HOME);
+  const { currency } = useAppSelector((state) => state.settings);
 
   if (!isFeatureOn) {
     return null;
@@ -25,6 +30,24 @@ export default function HomePage() {
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-10">
             {/* Left Section: Financial & Account Overview */}
             <div className="col-span-1 md:col-span-2 lg:col-span-7 space-y-4">
+              <StackedBarChart
+                data={data2025}
+                title="2025"
+                currency={currency}
+                xAxisFormatter={(value) => formatCurrency(value, currency)}
+              />
+              <StackedBarChart
+                data={data2024}
+                title="2024"
+                currency={currency}
+                xAxisFormatter={(value) => formatCurrency(value, currency)}
+              />
+              <StackedBarChart
+                data={data2023}
+                title="2023"
+                currency={currency}
+                xAxisFormatter={(value) => formatCurrency(value, currency)}
+              />
               {isFeatureOn && <AccountDashboard module={MODULE.HOME} />}
               <AccountsOverview />
             </div>
