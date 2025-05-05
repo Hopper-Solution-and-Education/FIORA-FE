5cb42582c0f3a5df3b88021e0f0de28d185230ab
Quantaphocpython <quan02042004@gmail.com>
2025-05-05 18:10:11 +0700

    feat: update tenative budget

--- src/features/setting/api/domain/use-cases/budgetSummaryUsecase.ts
+++ src/features/setting/api/domain/use-cases/budgetSummaryUsecase.ts
@@ -1,6 +1,8 @@
-import { BudgetsTable, BudgetType } from '@prisma/client';
-import { IBudgetRepository } from '../../repositories/budgetRepository';
+import { BudgetsTable, BudgetType, Prisma } from '@prisma/client';
+import { ITransactionRepository } from '../../../../transaction/domain/repositories/transactionRepository.interface';
+import { transactionRepository } from '../../../../transaction/infrastructure/repositories/transactionRepository';
 import { budgetRepository } from '../../infrastructure/repositories/budgetProductRepository';
+import { IBudgetRepository } from '../../repositories/budgetRepository';
 
 interface BudgetSummaryResponse {
   topBudget: BudgetsTable | null;
@@ -10,7 +12,10 @@ interface BudgetSummaryResponse {
 }
 
 class BudgetSummaryUsecase {
-  constructor(private _budgetRepository: IBudgetRepository = budgetRepository) {}
+  constructor(
+    private _budgetRepository: IBudgetRepository = budgetRepository,
+    private _transactionRepository: ITransactionRepository = transactionRepository,
+  ) {}
 
   async getBudgetsByUserIdAndFiscalYear(
     userId: string,
@@ -42,7 +47,84 @@ class BudgetSummaryUsecase {
       userId,
       fiscalYear,
     );
-    return budgets.find((budget) => budget.type === type) || null;
+    const budget = budgets.find((budget) => budget.type === type) || null;
+
+    if (budget && type === BudgetType.Act) {
+      // get transaction from last 1 month ago to now
+      const now = new Date();
+      const oneMonthAgo = new Date();
+      oneMonthAgo.setMonth(now.getMonth() - 1);
+
+      const transactions = await this._transactionRepository.findManyTransactions({
+        userId,
+        date: {
+          gte: oneMonthAgo,
+          lte: now,
+        },
+        isDeleted: false,
+      });
+
+      let totalIncome = 0;
+      let totalExpense = 0;
+
+      const monthlyIncome: Record<number, number> = {};
+      const monthlyExpense: Record<number, number> = {};
+
+      for (let i = 1; i <= 12; i++) {
+        monthlyIncome[i] = 0;
+        monthlyExpense[i] = 0;
+      }
+
+      transactions.forEach((transaction) => {
+        const month = transaction.date.getMonth() + 1;
+
+        if (transaction.type === 'Income') {
+          totalIncome += Number(transaction.amount);
+          monthlyIncome[month] += Number(transaction.amount);
+        } else if (transaction.type === 'Expense') {
+          totalExpense += Number(transaction.amount);
+          monthlyExpense[month] += Number(transaction.amount);
+        }
+      });
+
+      budget.total_inc = new Prisma.Decimal(totalIncome);
+      budget.total_exp = new Prisma.Decimal(totalExpense);
+
+      for (let i = 1; i <= 12; i++) {
+        const incField = `m${i}_inc`;
+        const expField = `m${i}_exp`;
+
+        (budget as any)[incField] = new Prisma.Decimal(monthlyIncome[i]);
+        (budget as any)[expField] = new Prisma.Decimal(monthlyExpense[i]);
+      }
+
+      budget.q1_inc = new Prisma.Decimal(monthlyIncome[1] + monthlyIncome[2] + monthlyIncome[3]);
+      budget.q1_exp = new Prisma.Decimal(monthlyExpense[1] + monthlyExpense[2] + monthlyExpense[3]);
+      budget.q2_inc = new Prisma.Decimal(monthlyIncome[4] + monthlyIncome[5] + monthlyIncome[6]);
+      budget.q2_exp = new Prisma.Decimal(monthlyExpense[4] + monthlyExpense[5] + monthlyExpense[6]);
+      budget.q3_inc = new Prisma.Decimal(monthlyIncome[7] + monthlyIncome[8] + monthlyIncome[9]);
+      budget.q3_exp = new Prisma.Decimal(monthlyExpense[7] + monthlyExpense[8] + monthlyExpense[9]);
+      budget.q4_inc = new Prisma.Decimal(monthlyIncome[10] + monthlyIncome[11] + monthlyIncome[12]);
+      budget.q4_exp = new Prisma.Decimal(
+        monthlyExpense[10] + monthlyExpense[11] + monthlyExpense[12],
+      );
+
+      const q1Inc = Number(budget.q1_inc);
+      const q2Inc = Number(budget.q2_inc);
+      const q3Inc = Number(budget.q3_inc);
+      const q4Inc = Number(budget.q4_inc);
+      const q1Exp = Number(budget.q1_exp);
+      const q2Exp = Number(budget.q2_exp);
+      const q3Exp = Number(budget.q3_exp);
+      const q4Exp = Number(budget.q4_exp);
+
+      budget.h1_inc = new Prisma.Decimal(q1Inc + q2Inc);
+      budget.h1_exp = new Prisma.Decimal(q1Exp + q2Exp);
+      budget.h2_inc = new Prisma.Decimal(q3Inc + q4Inc);
+      budget.h2_exp = new Prisma.Decimal(q3Exp + q4Exp);
+    }
+
+    return budget;
   }
 }
 
