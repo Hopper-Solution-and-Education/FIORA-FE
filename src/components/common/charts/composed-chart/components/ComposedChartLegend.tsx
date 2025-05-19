a9a300b57c50a3d012fa52a252664ed6d4d89656
Kaitou <121939559+Quantaphocpython@users.noreply.github.com>
2025-05-19 21:44:09 +0700

    feat: update line from strict to optional field and update style for 0 line (#358)
    * refactor: refactor chart folder
    
    * feat: vertical positive negative bar chart
    
    * refactor: refactor chart props
    
    * feat: update line from strict to optional field and update style for 0 line

--- src/components/common/charts/composed-chart/components/ComposedChartLegend.tsx
+++ src/components/common/charts/composed-chart/components/ComposedChartLegend.tsx
@@ -2,7 +2,7 @@ import { ColumnConfig, LineConfig } from '@/shared/types/chart.type';
 
 interface CustomLegendProps {
   columns: ColumnConfig[];
-  lines: LineConfig[];
+  lines?: LineConfig[];
 }
 
 export const renderCustomLegend = ({ columns, lines }: CustomLegendProps) => (
@@ -13,11 +13,12 @@ export const renderCustomLegend = ({ columns, lines }: CustomLegendProps) => (
         <span className="text-sm text-gray-600 dark:text-gray-400">{column.name}</span>
       </div>
     ))}
-    {lines.map((line, index) => (
-      <div key={`legend-line-${index}`} className="flex items-center">
-        <div className="w-3 h-3 mr-2 rounded-sm" style={{ backgroundColor: line.color }} />
-        <span className="text-sm text-gray-600 dark:text-gray-400">{line.name}</span>
-      </div>
-    ))}
+    {lines &&
+      lines.map((line, index) => (
+        <div key={`legend-line-${index}`} className="flex items-center">
+          <div className="w-3 h-3 mr-2 rounded-sm" style={{ backgroundColor: line.color }} />
+          <span className="text-sm text-gray-600 dark:text-gray-400">{line.name}</span>
+        </div>
+      ))}
   </div>
 );
