605c66900d0920582014e4fb631ddcb99cd956b9
Quantaphocpython <quan02042004@gmail.com>
2025-05-20 13:44:17 +0700

    refactor: refactor finance usecase

--- src/features/setting/api/domain/use-cases/financeUsecase.ts
+++ src/features/setting/api/domain/use-cases/financeUsecase.ts
@@ -1,8 +1,16 @@
-import { financeRepository } from '../../infrastructure/repositories/financeRepository';
-import { IFinanceRepository } from '../../repositories/financeRepository.interface';
+import { IAccountRepository } from '@/features/auth/domain/repositories/accountRepository.interface';
+import { categoryRepository } from '../../infrastructure/repositories/categoryRepository';
+import { ICategoryRepository } from '../../repositories/categoryRepository.interface';
+import { accountRepository } from '@/features/auth/infrastructure/repositories/accountRepository';
+import { IProductRepository } from '../../repositories/productRepository.interface';
+import { productRepository } from '../../infrastructure/repositories/productRepository';
 
 export class FinanceUseCase {
-  constructor(private _financeRepository: IFinanceRepository = financeRepository) {}
+  constructor(
+    private _categoryRepository: ICategoryRepository = categoryRepository,
+    private _accountRepository: IAccountRepository = accountRepository,
+    private _productRepository: IProductRepository = productRepository,
+  ) {}
 }
 
 export const financeUseCase = new FinanceUseCase();
