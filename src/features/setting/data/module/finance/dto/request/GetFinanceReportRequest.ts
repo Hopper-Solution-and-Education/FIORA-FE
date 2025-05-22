05a356cf3abff978cfa6b83a10f47d470979a336
Quantaphocpython <quan02042004@gmail.com>
2025-05-22 21:38:14 +0700

    fix: fix param validated message

--- src/features/setting/data/module/finance/dto/request/GetFinanceReportRequest.ts
+++ src/features/setting/data/module/finance/dto/request/GetFinanceReportRequest.ts
@@ -3,7 +3,7 @@ import { FinanceReportEnum } from '../../constant/FinanceReportEnum';
 import { FinanceReportFilterEnum } from '../../constant/FinanceReportFilterEnum';
 
 export const GetFinanceReportSchema = yup.object().shape({
-  type: yup.string().oneOf(Object.values(FinanceReportEnum)).required('Loại báo cáo là bắt buộc'),
+  type: yup.string().oneOf(Object.values(FinanceReportEnum)).required('Report type is required'),
   filter: yup
     .string()
     .oneOf(Object.values(FinanceReportFilterEnum))
