307c29c3b034caae61cec807839f38fbb09be8ae
Kien Nguyen <trungkiennguyen0310@gmail.com>
2025-06-06 21:23:59 +0700

    feat: init membership chart

--- src/features/setting/module/membership/presentation/molecules/MembershipRankChart.tsx
+++ src/features/setting/module/membership/presentation/molecules/MembershipRankChart.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
+import LevelChart from '@/components/common/charts/scatter-chart';
 
 const MembershipRankChart = () => {
   return (
-    <div className="p-4 rounded-lg shadow col-span-3">
-      <h2 className="text-xl font-bold mb-4">BALANCE</h2>
-      <div className="h-64 flex items-center justify-center text-gray-400">
-        [Balance Graph Placeholder]
-      </div>
+    <div className="shadow col-span-3">
+      <LevelChart />
     </div>
   );
 };
