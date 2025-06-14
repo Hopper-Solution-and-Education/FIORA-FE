ecc710a66a2c4af5c967aafe5e7dd61b7cc05023
KrissTiNa207 <99896326+TriSBie@users.noreply.github.com>
2025-06-14 10:56:59 +0700

    fix: add validate configuration body and enhance others api logic

--- src/pages/api/categories/expense-income.ts
+++ src/pages/api/categories/expense-income.ts
@@ -1,8 +1,11 @@
 import { categoryUseCase } from '@/features/setting/api/domain/use-cases/categoryUseCase';
 import { Messages } from '@/shared/constants/message';
 import RESPONSE_CODE from '@/shared/constants/RESPONSE_CODE';
+import { createErrorResponse } from '@/shared/lib';
 import { createError, createResponse } from '@/shared/lib/responseUtils/createResponse';
 import { sessionWrapper } from '@/shared/utils/sessionWrapper';
+import { validateBody } from '@/shared/utils/validate';
+import { categoryBodySchema } from '@/shared/validators/categoryValidator';
 import { CategoryType } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
@@ -45,6 +48,12 @@ export async function GET(req: NextApiRequest, res: NextApiResponse, userId: str
 export async function POST(req: NextApiRequest, res: NextApiResponse, userId: string) {
   try {
     const { name, type, icon, description, parentId } = req.body;
+    const { error } = validateBody(categoryBodySchema, req.body);
+    if (error) {
+      return res
+        .status(RESPONSE_CODE.BAD_REQUEST)
+        .json(createErrorResponse(RESPONSE_CODE.BAD_REQUEST, Messages.VALIDATION_ERROR, error));
+    }
     const newCategory = await categoryUseCase.createCategory({
       userId,
       type: type as CategoryType,
