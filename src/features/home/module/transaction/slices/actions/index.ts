f0e21291e802a41ffe35c82a68f25f6a396bcca3
Luong Hoang Anh <hoanganh.dev.it@gmail.com>
2025-07-02 21:42:37 +0700

    NO-TICKET: [Refactor] Update data handling logics for Transaction page (#590)
    * feat: init UI for exchange rate setting
    
    * feat: exchange rate setting API integration and input validating
    
    * fix: fix build issue
    
    * fix: update logics of data handling for Transaction

---
+++ src/features/home/module/transaction/slices/actions/index.ts
@@ -0,0 +1,63 @@
+import { ITransactionPaginatedResponse } from '@/features/home/module/transaction/types';
+import transactionServices from '@/features/home/services/transactionServices';
+import { Transaction } from '@/features/setting/module/product/domain/entities/Transaction';
+import { FilterCriteria } from '@/shared/types';
+import { Response } from '@/shared/types/Common.types';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchTransactions = createAsyncThunk(
+  'transaction/fetchTransactions',
+  async (filterCriteria: FilterCriteria, { rejectWithValue }) => {
+    try {
+      const response: Response<ITransactionPaginatedResponse> =
+        await transactionServices.getFilteredTransactions(filterCriteria);
+      return response;
+    } catch (error: any) {
+      return rejectWithValue({
+        message: error.message || 'Failed to fetch categories! Please try again!',
+      });
+    }
+  },
+);
+
+export const deleteTransaction = createAsyncThunk(
+  'transaction/deleteTransaction',
+  async (id: string, { rejectWithValue }) => {
+    try {
+      await transactionServices.deleteTransaction(id);
+      return id;
+    } catch (error: any) {
+      return rejectWithValue({
+        message: error.message || 'Failed to delete transaction! Please try again!',
+      });
+    }
+  },
+);
+
+export const getTransactionById = createAsyncThunk(
+  'transaction/getTransactionById',
+  async (id: string, { rejectWithValue }) => {
+    try {
+      const response: Response<Transaction> = await transactionServices.getTransactionById(id);
+      return response;
+    } catch (error: any) {
+      return rejectWithValue({
+        message: error.message || 'Failed to get transaction by id! Please try again!',
+      });
+    }
+  },
+);
+
+export const updateTransaction = createAsyncThunk(
+  'transaction/updateTransaction',
+  async ({ id, data }: { id: string; data: Partial<Transaction> }, { rejectWithValue }) => {
+    try {
+      const response: Response<Transaction> = await transactionServices.updateTransaction(id, data);
+      return response;
+    } catch (error: any) {
+      return rejectWithValue({
+        message: error.message || 'Failed to update transaction! Please try again!',
+      });
+    }
+  },
+);
