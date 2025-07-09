fa937725bbc22510254881ebdb4fbbd4518b368c
Nguyễn Huy Khải <kai.nguyen@terralogic.com>
2025-07-09 17:01:28 +0700

    feat: update api call budget categories

--- src/pages/api/categories/index.ts
+++ src/pages/api/categories/index.ts
@@ -6,7 +6,7 @@ import { createError, createResponse } from '@/shared/lib/responseUtils/createRe
 import { sessionWrapper } from '@/shared/utils/sessionWrapper';
 import { validateBody } from '@/shared/utils/validate';
 import { categoryBodySchema } from '@/shared/validators/categoryValidator';
-import { CategoryType } from '@prisma/client';
+import { CategoryType, Currency } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export const maxDuration = 30; // 30 seconds
@@ -26,9 +26,13 @@ export default sessionWrapper(async (req, res, userId) => {
 
 export async function GET(req: NextApiRequest, res: NextApiResponse, userId: string) {
   try {
-    const { type, year } = req.query as { type: CategoryType; year: string };
+    const { type, year } = req.query as {
+      type: CategoryType;
+      year: string;
+    };
+    const currency = (req.headers['x-user-currency'] as string as Currency) ?? Currency.VND;
 
-    const categories = await categoryUseCase.getListCategoryByType(userId, type, year);
+    const categories = await categoryUseCase.getListCategoryByType(userId, type, year, currency);
 
     res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     res.setHeader('Pragma', 'no-cache');
