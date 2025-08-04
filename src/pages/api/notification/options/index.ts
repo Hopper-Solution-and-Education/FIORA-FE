7245772e60e8e69f75c6b7c4e4b06f57f1f80359
RankillerDY <rankillerdy@gmail.com>
2025-08-04 22:23:48 +0700

    feat: handle error more proper in be - refactor

--- src/pages/api/notification/options/index.ts
+++ src/pages/api/notification/options/index.ts
@@ -1,39 +1,33 @@
 import { notificationUseCase } from '@/features/notification/application/use-cases/notificationUseCase';
 import { Messages } from '@/shared/constants/message';
 import RESPONSE_CODE from '@/shared/constants/RESPONSE_CODE';
-import { createError, createResponse } from '@/shared/lib/responseUtils/createResponse';
+import { errorHandler } from '@/shared/lib';
+import { createResponse } from '@/shared/lib/responseUtils/createResponse';
 import { withAuthorization } from '@/shared/utils/authorizationWrapper';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default withAuthorization({
   GET: ['Admin'],
-})(async (req: NextApiRequest, res: NextApiResponse) => {
-  switch (req.method) {
-    case 'GET':
-      return GET(req, res);
-
-    default:
-      return res
-        .status(RESPONSE_CODE.METHOD_NOT_ALLOWED)
-        .json({ error: Messages.METHOD_NOT_ALLOWED });
-  }
-});
+})((req: NextApiRequest, res: NextApiResponse) =>
+  errorHandler(
+    async (request, response) => {
+      switch (request.method) {
+        case 'GET':
+          return GET(request, response);
+        default:
+          return response
+            .status(RESPONSE_CODE.METHOD_NOT_ALLOWED)
+            .json({ error: 'Method is not allowed' });
+      }
+    },
+    req,
+    res,
+  ),
+);
 
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const result = await notificationUseCase.getNotificationFilterOptions();
-    return res
-      .status(RESPONSE_CODE.OK)
-      .json(createResponse(RESPONSE_CODE.OK, Messages.GET_NOTIFICATION_SUCCESS, result));
-  } catch (error: any) {
-    return res
-      .status(RESPONSE_CODE.INTERNAL_SERVER_ERROR)
-      .json(
-        createError(
-          res,
-          RESPONSE_CODE.INTERNAL_SERVER_ERROR,
-          error.message || Messages.INTERNAL_ERROR,
-        ),
-      );
-  }
+  const result = await notificationUseCase.getNotificationFilterOptions();
+  return res
+    .status(RESPONSE_CODE.OK)
+    .json(createResponse(RESPONSE_CODE.OK, Messages.GET_NOTIFICATION_SUCCESS, result));
 }
