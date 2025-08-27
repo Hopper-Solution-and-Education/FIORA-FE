a8cd2aa990963045ba2298446d62e303d8b8cfef
Jacob Nguyen <jacob.nguyen@terralogicvietnam.com>
2025-08-27 16:21:05 +0700

    feat: add filter dashboard

--- src/features/setting/api/infrastructure/repositories/bankAccountRepository.ts
+++ src/features/setting/api/infrastructure/repositories/bankAccountRepository.ts
@@ -78,6 +78,9 @@ class BankAccountRepository {
       where: {
         userId: id,
       },
+      include: {
+        Attachment: true,
+      },
     });
   }
 
---
+++ src/features/setting/api/infrastructure/types/dashboard.types.ts
@@ -0,0 +1,16 @@
+import { CronJobStatus, TypeCronJob } from '@prisma/client';
+
+export interface DashboardFilterParams {
+  status?: CronJobStatus | CronJobStatus[];
+  typeCronJob?: TypeCronJob | TypeCronJob[];
+  search?: string;
+  fromDate?: string;
+  toDate?: string;
+  createdBy?: string | string[];
+  updatedBy?: string | string[];
+  page?: number;
+  limit?: number;
+  sortBy?: string;
+  sortOrder?: 'asc' | 'desc';
+  userIds?: string | string[];
+}
