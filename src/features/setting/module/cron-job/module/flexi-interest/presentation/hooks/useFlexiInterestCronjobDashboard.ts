5eeb95379c8094e3e8574b7877a9ea10edd779fd
haidang9403 <109220759+haidang9403@users.noreply.github.com>
2025-09-23 20:09:42 +0700

    fix: remove log

--- src/features/setting/module/cron-job/module/flexi-interest/presentation/hooks/useFlexiInterestCronjobDashboard.ts
+++ src/features/setting/module/cron-job/module/flexi-interest/presentation/hooks/useFlexiInterestCronjobDashboard.ts
@@ -49,7 +49,6 @@ export const useFlexiInterestCronjobDashboard = () => {
           pageSize,
           filter: JSON.stringify(formattedFilter),
         }).unwrap();
-        console.log('🚀 ~ fetchData ~ response:', response);
 
         const rows: FlexiInterestCronjobTableData[] = response?.items || [];
 
