f3e18f50e530d2bca0a1a0077dafb55962a1b680
lamnhuthuy <lamnhuthuy.contact@gmail.com>
2025-09-24 22:02:46 +0700

    [BE] Savings -> Savings Dashboard #927

---
+++ src/pages/api/wallet/smart-saving/transfer.ts
@@ -0,0 +1,50 @@
+import { walletUseCase } from '@/features/setting/api/domain/use-cases/walletUsecase';
+import { Messages } from '@/shared/constants/message';
+import RESPONSE_CODE from '@/shared/constants/RESPONSE_CODE';
+import { createError, createResponse } from '@/shared/lib/responseUtils/createResponse';
+import { createErrorResponse } from '@/shared/lib/utils';
+import { sessionWrapper } from '@/shared/utils/sessionWrapper';
+import { validateBody } from '@/shared/utils/validate';
+import { transferSavingWalletBodySchema } from '@/shared/validators/savingWalletValidator';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default sessionWrapper(async (req: NextApiRequest, res: NextApiResponse, userId: string) => {
+  try {
+    switch (req.method) {
+      case 'POST':
+        return POST(req, res, userId);
+      default:
+        return createError(res, RESPONSE_CODE.METHOD_NOT_ALLOWED, Messages.METHOD_NOT_ALLOWED);
+    }
+  } catch (error: any) {
+    return createError(
+      res,
+      RESPONSE_CODE.INTERNAL_SERVER_ERROR,
+      error.message || Messages.INTERNAL_ERROR,
+    );
+  }
+});
+
+async function POST(req: NextApiRequest, res: NextApiResponse, userId: string) {
+  try {
+    const { error } = validateBody(transferSavingWalletBodySchema, req.body);
+    if (error) {
+      return res
+        .status(RESPONSE_CODE.BAD_REQUEST)
+        .json(createErrorResponse(RESPONSE_CODE.BAD_REQUEST, Messages.VALIDATION_ERROR, error));
+    }
+    const { packageFXId, action } = req.body;
+
+    const result = await walletUseCase.transferSavingWallet(userId, packageFXId, action);
+
+    return res
+      .status(RESPONSE_CODE.OK)
+      .json(createResponse(RESPONSE_CODE.OK, Messages.CREATE_TRANSACTION_SUCCESS, result));
+  } catch (error: any) {
+    return createError(
+      res,
+      RESPONSE_CODE.INTERNAL_SERVER_ERROR,
+      error.message || Messages.INTERNAL_ERROR,
+    );
+  }
+}
