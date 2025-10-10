b18dfe7b9d409472260bbd7085765900eea9c9ee
Kai Nguyen <huykhai.work@gmail.com>
2025-10-10 19:23:25 +0700

    style: update ui view

--- src/components/common/atoms/StackYAxisTick.tsx
+++ src/components/common/atoms/StackYAxisTick.tsx
@@ -1,9 +1,9 @@
 'use client';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { IconDisplay } from '@/components/common/atoms/IconDisplay';
+import { throttle } from 'lodash';
 import type React from 'react';
 import { memo, useState } from 'react';
-import { throttle } from 'lodash';
-import { IconDisplay } from '@/components/common/atoms/IconDisplay';
+import { CommonTooltip } from './CommonTooltip';
 
 const THROTTLE_DELAY = 300;
 
@@ -46,35 +46,22 @@ const StackYAxisTick: React.FC<StackYAxisTick> = ({
 
   return (
     <g transform={`translate(${x},${y})`}>
-      <TooltipProvider>
-        <Tooltip open={isIconHovered}>
-          <TooltipTrigger asChild>
-            <foreignObject
-              x={-40}
-              y={-16}
-              width={32}
-              height={32}
-              onMouseEnter={() => setIsIconHovered(true)}
-              onMouseLeave={() => setIsIconHovered(false)}
-            >
-              <IconDisplay
-                icon={item?.icon || 'activity'}
-                isHovered={isIconHovered}
-                onClick={handleIconClick}
-              />
-            </foreignObject>
-          </TooltipTrigger>
-          <TooltipContent
-            side="top"
-            align="center"
-            className="text-xs p-1.5 bg-popover/95 backdrop-blur-sm border-border/50 shadow-md"
-          >
-            <div className="flex flex-col gap-1">
-              <span className="font-medium text-gray-900 dark:text-gray-300">{payload.value}</span>
-            </div>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      <CommonTooltip content={payload.value}>
+        <foreignObject
+          x={-40}
+          y={-16}
+          width={32}
+          height={32}
+          onMouseEnter={() => setIsIconHovered(true)}
+          onMouseLeave={() => setIsIconHovered(false)}
+        >
+          <IconDisplay
+            icon={item?.icon || 'activity'}
+            isHovered={isIconHovered}
+            onClick={handleIconClick}
+          />
+        </foreignObject>
+      </CommonTooltip>
     </g>
   );
 };
