f6ed059ef90716c022f1f7e516aa0e36501d5949
Bùi Nhật Đức Anh <124678780+buinhatducanh@users.noreply.github.com>
2025-11-05 15:56:24 +0700

    feat: update hook for logic form

--- src/features/sending/presentation/views/SendingFXForm.tsx
+++ src/features/sending/presentation/views/SendingFXForm.tsx
@@ -1,94 +1,26 @@
 'use client';
 
 import { GlobalDialog } from '@/components/common/molecules';
-import { ApiEndpointEnum } from '@/shared/constants/ApiEndpointEnum';
-import useDataFetch from '@/shared/hooks/useDataFetcher';
-import type { WalletSendingOverview } from '@/shared/types/otp';
-import { useAppSelector } from '@/store';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { useSendingFXAction } from '../../utils/actions';
+import { useSendingFXFormLogic } from '../../hooks/useSendingFXFormLogic';
 import SendingFXFormContent from './SendingFXFormContent';
 
-import { SendingInputs, sendingSchema } from '../../utils/validation';
-
-type OverviewSendingResponseType = { data: WalletSendingOverview };
-
 export default function SendingFXForm() {
-  const { isShowSendingFXForm, wallets } = useAppSelector((s) => s.wallet);
-  const { currency } = useAppSelector((s) => s.settings);
   const {
+    isShowSendingFXForm,
+    isGlobalLoading,
+    currency,
     limit,
-    isFetched: isLimitFetched,
-    isLoading: isLimitLoading,
-  } = useAppSelector((s) => s.limitData);
-  const {
-    catalog,
-    isFetched: isCatalogFetched,
-    isLoading: isCatalogLoading,
-  } = useAppSelector((s) => s.catalogData);
-
-  const { categories, products } = catalog || { categories: [], products: [] };
-
-  const { isLoading: isOverviewLoading } = useDataFetch<OverviewSendingResponseType>({
-    endpoint: ApiEndpointEnum.walletWithdraw,
-    method: 'GET',
-    refreshInterval: 1000 * 60 * 5,
-  });
-
-  const {
+    categories,
+    products,
     otpState,
     countdown,
-    isLoading: actionIsLoading,
-    handleGetOtp: actionHandleGetOtp,
-    handleSubmit: actionHandleSubmit,
-    handleClose: actionHandleClose,
-    checkAmountBalance,
-    getCurrentBalance,
-  } = useSendingFXAction();
-
-  const {
     control,
-    handleSubmit,
-    setError,
-    clearErrors,
-    reset,
-    formState: { errors },
-  } = useForm<SendingInputs>({
-    resolver: zodResolver(sendingSchema),
-    defaultValues: {
-      receiver: '',
-      categoryId: null,
-      productId: null,
-      amountInput: 0,
-      description: '',
-      otp: '',
-    },
-    mode: 'onChange',
-  });
-
-  const isGlobalLoading =
-    !wallets ||
-    wallets.length === 0 ||
-    (!isLimitFetched && isLimitLoading) ||
-    (!isCatalogFetched && isCatalogLoading) ||
-    isOverviewLoading ||
-    actionIsLoading;
-
-  const onGetOtp = handleSubmit((data) =>
-    actionHandleGetOtp(data, (amount: number) => checkAmountBalance(amount, setError, clearErrors)),
-  );
-
-  const onConfirm = handleSubmit((data) =>
-    actionHandleSubmit(
-      data,
-      (amount: number) => checkAmountBalance(amount, setError, clearErrors),
-      setError,
-      reset,
-    ),
-  );
-
-  const onClose = () => actionHandleClose(reset);
+    errors,
+    onGetOtp,
+    onConfirm,
+    onClose,
+    getCurrentBalance,
+  } = useSendingFXFormLogic();
 
   return (
     <GlobalDialog
