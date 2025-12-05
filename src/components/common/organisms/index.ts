854d263b04398b54032d4c414573ff54f12a54a2
Kai Nguyen <huykhai.work@gmail.com>
2025-12-06 03:49:46 +0700

    fix: generate slug membership benefit & help center module

--- src/components/common/organisms/index.ts
+++ src/components/common/organisms/index.ts
@@ -1,6 +1,7 @@
 export { default as ChartSkeleton } from './ChartSkeleton';
 export { default as CommonTable } from './CommonTable';
 export * from './DeleteDialog';
+export { default as EmptyState } from './EmptyState';
 export { default as SearchBar } from './SearchBar';
 export { default as StackedBarChartSkeleton } from './StackedBarChartSkeleton';
 export { default as TableSkeleton } from './TableSkeleton';
